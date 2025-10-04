356a2130c54f22608bb6cbd137d9fa1ba697fe37
QuyTran <trandinhquy080107@gmail.com>
2025-10-05 01:32:48 +0700

    Add 2 more lines to end the programme

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main() {
 	cout << "Hello, World!" << endl;
+	cout << "This is a sample C++ program." << endl;
+	cout << "C++14 features can be used here." << endl;
+	cout << "Goodbye!" << endl;
+	cout << "End of program." << endl;
 	return 0;
 }
